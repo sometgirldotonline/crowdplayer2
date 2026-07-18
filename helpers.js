424ff0232ed70f7ae71902ee55d01923abc61a36
Lily <lily@sometgirl.online>
2026-07-18 23:05:48 +1000

    squeaky clean 🐁

---
+++ helpers.js
@@ -0,0 +1,60 @@
+if (!navigator.storage && !navigator.storage.estimate) {
+    alert(`Sorry! 
+It seems your browser might be too old for some of our functionality, maybe check for updates?
+If you're just controlling music and don't want to send over any of your own files, you can disregard this message
+Error code: ENoNavStor`)
+}
+async function writeJsonToOpfs(fileName, jsonObject) {
+    const root = await navigator.storage.getDirectory();
+    const fileHandle = await root.getFileHandle(fileName, { create: true });
+    const writable = await fileHandle.createWritable();
+    const jsonString = JSON.stringify(jsonObject, null, 2);
+    await writable.write(jsonString);
+    await writable.close();
+    showQuota()
+    console.log(`${fileName} written successfully.`);
+}
+async function readJsonFromOpfs(fileName) {
+    try {
+        const root = await navigator.storage.getDirectory();
+        const fileHandle = await root.getFileHandle(fileName);
+        const file = await fileHandle.getFile();
+        const text = await file.text();
+        const jsonObject = JSON.parse(text);
+        return jsonObject;
+    } catch (error) {
+        console.error(`Error reading ${fileName}:`, error);
+        return {};
+    }
+}
+async function showQuota() {
+    try {
+        const estimate = await navigator.storage.estimate();
+        const quotaInMB = (estimate.quota / (1024 * 1024)).toFixed(0);
+        const totalUsedInMB = (estimate.usage / (1024 * 1024)).toFixed(0);
+        document.querySelector("#quota").innerText = `${totalUsedInMB}MB / ${quotaInMB}MB Used`
+    }
+    catch (e) {
+        alert(`Error calculating storage usage
+Error code: ${e}`)
+            console.error(e)
+    }
+}
+async function purgeCache() {
+    const root = await navigator.storage.getDirectory();
+    const fileHandle = await root.getFileHandle("meta.json", { create: true });
+    const fileHandle2 = await root.getFileHandle("library.json", { create: true });
+    const fileSz = Math.round((await fileHandle.getFile()).size / (1e+6)) + Math.round((await fileHandle2.getFile()).size / (1e+6))
+    await writeJsonToOpfs("meta.json", {})
+    await writeJsonToOpfs("library.json", {})
+    alert(`Cleared ${fileSz}MB`)
+    showQuota()
+}
+
+async function sha256(string) {
+    const msgBuffer = new TextEncoder().encode(string);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    return hashHex;
+}
--- host/host.js
+++ host/host.js
@@ -1,69 +1,26 @@
-if (!navigator.storage && !navigator.storage.estimate) {
-    alert(`Sorry! 
-It seems your browser might be too old for some of our functionality, maybe check for updates? (if you're on an EOL operating system like windows 8 or older, try Supermium, it's Google Chrome but with updates from newer windows versions!)\
-
-Error code: ENoNavStor`)
-}
-async function showQuota() {
-    try {
-        const estimate = await navigator.storage.estimate();
-        const quotaInMB = (estimate.quota / (1024 * 1024)).toFixed(0);
-        const totalUsedInMB = (estimate.usage / (1024 * 1024)).toFixed(0);
-        document.querySelector("#quota").innerText = `${totalUsedInMB}MB / ${quotaInMB}MB Used`
-    }
-    catch (e) {
-        alert(`Error calculating storage usage
-Error code: ${e}`)
-            console.error(e)
-    }
-}
-showQuota()
 let button_setup_pickFolder = document.querySelector("#setup-pick-folder")
 let button_setup_Continue = document.querySelector("#setup-continue")
 let button_setup_Process = document.querySelector("#setup-process-folder")
+
+showQuota()
+
 let musicFolder = null
 let musicFiles = {}
-async function writeJsonToOpfs(fileName, jsonObject) {
-    const root = await navigator.storage.getDirectory();
-    const fileHandle = await root.getFileHandle(fileName, { create: true });
-    const writable = await fileHandle.createWritable();
-    const jsonString = JSON.stringify(jsonObject, null, 2);
-    await writable.write(jsonString);
-    await writable.close();
-    showQuota()
-    console.log(`${fileName} written successfully.`);
-}
-async function readJsonFromOpfs(fileName) {
-    try {
-        const root = await navigator.storage.getDirectory();
-        const fileHandle = await root.getFileHandle(fileName);
-        const file = await fileHandle.getFile();
-        const text = await file.text();
-        const jsonObject = JSON.parse(text);
-        return jsonObject;
-    } catch (error) {
-        console.error(`Error reading ${fileName}:`, error);
-        return {};
-    }
-}
 let musicFilesMeta = {}
+let library = {}
+let musicFileFormats = ["mp3", "m4a", "aac", "wav", "flac", "ogg", "opus", "webm"]
+
+
 readJsonFromOpfs("meta.json").then(meta => {
     musicFilesMeta = meta
 }).catch(() => {
     musicFilesMeta = {}
 })
-if (musicFilesMeta == null){
+if (musicFilesMeta == null) {
     musicFilesMeta = {}
 }
-let library = {}
-let musicFileFormats = ["mp3", "m4a", "aac", "wav", "flac", "ogg", "opus", "webm"]
-async function sha256(string) {
-    const msgBuffer = new TextEncoder().encode(string);
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-    return hashHex;
-}
+
+
 async function walkFiles(folder, path = "/") {
     for await (const entry of folder.values()) {
         console.log(entry.kind, entry.name);
@@ -93,13 +50,14 @@ button_setup_pickFolder.addEventListener("click", async () => {
 
     }
     catch (e) {
-            console.error(e)
+        console.error(e)
         alert(`Sorry! 
-It seems your browser might be too old for some of our functionality, maybe check for updates? (if you're on an EOL operating system like windows 8 or older, try Supermium, it's Google Chrome but with updates from newer windows versions!)
-
+It seems your browser might be too old for some of our functionality, maybe check for updates? 
 Error code: ${e}`)
     }
 })
+
+
 button_setup_Process.addEventListener("click", async () => {
     try {
         button_setup_Process.setAttribute("disabled", "")
@@ -119,7 +77,7 @@ button_setup_Process.addEventListener("click", async () => {
                     musicFilesMeta[path] = id3
                     console.log(id3);
                 } catch (err) {
-            console.error(e)
+                    console.error(e)
                     console.warn("Skipping (parse failed):", musicFiles[i].name, err);
                 }
             }
@@ -144,17 +102,16 @@ button_setup_Process.addEventListener("click", async () => {
             document.querySelector("#parseprocess").value = i + 1;
             i++;
         }
-        writeJsonToOpfs("library.json",library)
+        writeJsonToOpfs("library.json", library)
         console.log("done")
         button_setup_Continue.removeAttribute("disabled")
         document.querySelector("#parseprocess").setAttribute("hidden", "")
 
     }
     catch (e) {
-            console.error(e)
+        console.error(e)
 
         alert(`An error occured
-
 Error code: ${e}`)
     }
     button_setup_Process.removeAttribute("disabled", "")
@@ -165,13 +122,3 @@ button_setup_Continue.addEventListener("click", () => {
     window.location.href = "./host/player/"
 })
 
-async function purgeCache() {
-    const root = await navigator.storage.getDirectory();
-    const fileHandle = await root.getFileHandle("meta.json", { create: true });
-    const fileHandle2 = await root.getFileHandle("library.json", { create: true });
-    const fileSz = Math.round((await fileHandle.getFile()).size / (1e+6)) + Math.round((await fileHandle2.getFile()).size / (1e+6))
-    await writeJsonToOpfs("meta.json", {})
-    await writeJsonToOpfs("library.json", {})
-    alert(`Cleared ${fileSz}MB`)
-    showQuota()
-}
