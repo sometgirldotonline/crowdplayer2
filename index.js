80ddead57477eac8dccb30c74f9f36ae8c497d78
Lily <lily@sometgirl.online>
2026-07-18 22:54:25 +1000

    fah

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ button_setup_pickFolder = document.querySelector("#enter-host")
 button_setup_Continue = document.querySelector("#enter-party")
 
 button_setup_pickFolder.addEventListener("click",()=>{
-    window.location.href = "./host"
+    window.location.href = "./host/"
 })
 
 button_setup_Continue.addEventListener("click",()=>{
-    window.location.href = "./join"
+    window.location.href = "./join/"
 })
